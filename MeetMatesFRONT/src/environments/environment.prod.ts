e269a78094cf108e18894c1d30a7ee3b0f885c15
Pilia Ennio <pilia.ennio@gmail.com>
2026-04-29 13:04:33 +0200

    http

--- MeetMatesFRONT/src/environments/environment.prod.ts
+++ MeetMatesFRONT/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-   apiUrl: 'https://meetmates-1.onrender.com'
+    apiUrl: 'https://meetmates-y0vl.onrender.com'
+
 
 };
--- MeetMatesFRONT/src/environments/environment.ts
+++ MeetMatesFRONT/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-   apiUrl: 'https://meetmates-1.onrender.com'
+  apiUrl: 'https://meetmates-y0vl.onrender.com'
+
 };
