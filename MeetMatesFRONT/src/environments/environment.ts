38a03314a447e427bb6e7841a4c647d5bc2b79a3
Pilia Ennio <pilia.ennio@gmail.com>
2026-04-30 11:47:05 +0200

    cookie

--- MeetMatesFRONT/src/environments/environment.ts
+++ MeetMatesFRONT/src/environments/environment.ts
@@ -1,5 +1,5 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://meetmates-y0vl.onrender.com'
 
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080'
 };
