f0003894ac5433a99927ca06efe64d81c31fe702
Pilia Ennio <pilia.ennio@gmail.com>
2026-05-16 17:49:58 +0200

    responsiv done

--- MeetMatesFRONT/src/app/shared-components/pipes/statusColor.pipe.spec.ts
+++ MeetMatesFRONT/src/app/shared-components/pipes/statusColor.pipe.spec.ts
@@ -12,52 +12,52 @@ describe('StatusColorPipe', () => {
   });
 
   describe('activity statuses', () => {
-    it('should map "ouvert" to green', () => {
-      expect(pipe.transform('ouvert')).toBe('text-green-600');
+    it('should map "ouvert" to success', () => {
+      expect(pipe.transform('ouvert')).toBe('status-success');
     });
 
-    it('should map "complet" to orange', () => {
-      expect(pipe.transform('complet')).toBe('text-orange-500');
+    it('should map "complet" to warning', () => {
+      expect(pipe.transform('complet')).toBe('status-warning');
     });
 
-    it('should map "annulé" to red', () => {
-      expect(pipe.transform('annulé')).toBe('text-red-600');
+    it('should map "annulé" to error', () => {
+      expect(pipe.transform('annulé')).toBe('status-error');
     });
 
-    it('should map "terminé" to gray', () => {
-      expect(pipe.transform('terminé')).toBe('text-gray-500');
+    it('should map "terminé" to neutral', () => {
+      expect(pipe.transform('terminé')).toBe('status-neutral');
     });
   });
 
   describe('participation statuses', () => {
-    it('should map "accepté" to green', () => {
-      expect(pipe.transform('accepté')).toBe('text-green-600');
+    it('should map "accepté" to success', () => {
+      expect(pipe.transform('accepté')).toBe('status-success');
     });
 
-    it('should map "en attente" to orange', () => {
-      expect(pipe.transform('en attente')).toBe('text-orange-500');
+    it('should map "en attente" to warning', () => {
+      expect(pipe.transform('en attente')).toBe('status-warning');
     });
 
-    it('should map "refusé" to red', () => {
-      expect(pipe.transform('refusé')).toBe('text-red-600');
+    it('should map "refusé" to error', () => {
+      expect(pipe.transform('refusé')).toBe('status-error');
     });
   });
 
   describe('edge cases', () => {
     it('should be case insensitive', () => {
-      expect(pipe.transform('OUVERT')).toBe('text-green-600');
+      expect(pipe.transform('OUVERT')).toBe('status-success');
     });
 
     it('should return default color for unknown value', () => {
-      expect(pipe.transform('foo')).toBe('text-black');
+      expect(pipe.transform('foo')).toBe('status-default');
     });
 
     it('should return default color for null', () => {
-      expect(pipe.transform(null)).toBe('text-black');
+      expect(pipe.transform(null)).toBe('status-default');
     });
 
     it('should return default color for undefined', () => {
-      expect(pipe.transform(undefined)).toBe('text-black');
+      expect(pipe.transform(undefined)).toBe('status-default');
     });
   });
 });
